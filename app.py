f1291199f18e19834d1a56e6ff1dcd7d1376ba93
Cagdas Polat <51061196+ekimdusu@users.noreply.github.com>
2021-08-28 01:53:11 +0300

    v0.1
    Four function for first prototype:
    1. Text -> Speech
    2. Speech -> Text
    3. Text + Translate -> Speech
    4. Speech + Translate -> Text
    
    It is a Streamlit project for now.

---
+++ app.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import speech_recognition as sr
+from gtts import gTTS 
+import os
+from os import path
+import subprocess
+import pydub as AudioSegment
+from google_trans_new import google_translator  
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+st.sidebar.title("ML çeviri denemeleri")
+menu = st.sidebar.radio("Ne yapmak istersiniz?", ("Yazıyı sese çeviri", "Yazıyı İngilizce sese çevir", "Sesi yazıya çeviri", "Sesi İngilizce yazıya çevir"))
+if menu == 'Yazıyı sese çeviri':
+    input = st.text_input("Ne söylememi istersin?")
+    if input != "":
+        tts = gTTS(text=input, lang='tr', slow=False)
+        tts.save("output.mp3")
+        audioFile = open('output.mp3', 'rb')
+        audioBytes = audioFile.read()
+        st.audio(audioBytes, format='audio/ogg',start_time=0)
+elif menu == 'Yazıyı İngilizce sese çevir':
+    input = st.text_input("Ne söylememi istersin?")
+    if input != "":
+        translator = google_translator()  
+        translate_text = translator.translate(input, lang_tgt='en')  
+        tts = gTTS(text=translate_text, lang='en', slow=False)
+        tts.save("output.mp3")
+        audioFile = open('output.mp3', 'rb')
+        audioBytes = audioFile.read()
+        st.audio(audioBytes, format='audio/ogg',start_time=0)
+elif menu == 'Sesi İngilizce yazıya çevir':
+    r = sr.Recognizer()
+    #st.write(sd.query_devices())
+    fs = 44100 
+    seconds = 5
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1, dtype='int16')
+    sd.wait()  
+    write('output.wav', fs, myrecording) 
+
+    with sr.AudioFile('/Users/hisuser/Desktop/AI/Project12/output.wav') as source:
+        audio = r.listen(source)  
+    try:
+        text = r.recognize_google(audio, language="tr-tr")
+        translator = google_translator()  
+        translate_text = translator.translate(text, lang_tgt='en')  
+        st.write(translate_text)
+    except sr.UnknownValueError:
+        st.write("Söylediğini anlamadım")
+elif menu == 'Sesi yazıya çeviri':
+    r = sr.Recognizer()
+    #st.write(sd.query_devices())
+    fs = 44100 
+    seconds = 5
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1, dtype='int16')
+    sd.wait()  
+    write('output.wav', fs, myrecording) 
+
+    with sr.AudioFile('/Users/hisuser/Desktop/AI/Project12/output.wav') as source:
+        audio = r.listen(source)  
+    try:
+        text = r.recognize_google(audio, language="tr-tr")
+        st.write(text)
+    except sr.UnknownValueError:
+        st.write("Söylediğini anlamadım")
